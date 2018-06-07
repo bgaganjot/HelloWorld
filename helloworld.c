bc68f8fd55a208f4b5d7c9377854b881b12d29eb
Gaganjot.Bhullar <bgaganjot@gmail.com>
2018-06-07 00:57:35 -0400

    Something

--- helloworld.c
+++ helloworld.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv){
-    FILE *f = fopen("/tmp/HelloWorld.txt", "w");
+    FILE *f = fopen("/tmp/HelloWorld.conf", "w");
     fprintf(f, "Hello World!");
     //printf("Hello World!");
     fclose(f);
